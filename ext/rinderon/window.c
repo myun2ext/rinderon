e75fb8cc6b33d91da605fec8d91a6589147d2f8e
myun2 <myun2@nwhite.info>
2014-09-09 14:29:07 +0900

    window.c add

---
+++ ext/rinderon/window.c
@@ -0,0 +1,15 @@
+#include "ruby.h"
+
+#define RB_MODULE					VALUE
+#define RB_CLASS					VALUE
+
+#define RINDERON_MODULE_NAME		"Rinderon"
+#define RINDERON_WINDOW_CLASS_NAME	"Window"
+#define MOD_RINDERON				rb_define_module(RINDERON_MODULE_NAME)
+
+#define DECL_RINDERON_CLASS(NAME)	rb_define_class_under(MOD_RINDERON, NAME, rb_cObject);
+
+RB_CLASS rinderon_window_class()
+{
+	DECL_RINDERON_CLASS(RINDERON_WINDOW_CLASS_NAME);
+}
